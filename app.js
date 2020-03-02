2bba2069ffd4ecb9607793448b325545509df5ba
William46503 <nextlevelpenguin@gmail.com>
2020-03-02 22:43:57 +0900

    Added funciton: - Search by City/Place

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=5de7292ed1cf4d0682e2354f9d9042fd"
+  res.sendFile(__dirname + "/index.html");
+
+  });
+
+app.post("/", function(req, res){
+  const placeName = req.body.cityName;
+  const appid = "5de7292ed1cf4d0682e2354f9d9042fd";
+  const unit = "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+placeName+"&units="+unit+"&appid="+appid;
 
   https.get(url, function(response){
     console.log(response.statusCode);
@@ -15,12 +27,11 @@ app.get("/", function(req, res){
         const iconURL = "http://openweathermap.org/img/wn/"+ weatherData.weather[0].icon + "@2x.png";
 
         res.write("<h1>The weather is " + weatherDescription +"</h1>");
-        res.write("<h2>The temperature in Tokyo is "+ temperature + " degree Celsius</h2>");
+        res.write("<h2>The temperature in "+ placeName +" is "+ temperature + " degree Celsius</h2>");
         res.write("<img src="+ iconURL + ">");
-        
+
         res.send();
     });
-
   });
 });
 
